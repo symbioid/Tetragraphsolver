eeb8315f0243401e79b319f7e0a0a56a0cc7273b
symbioid <symbioid@gmail.com>
2020-07-31 19:24:46 -0500

    Location: x,y of node;
    Target Matrix:
    -1 = up, left x/y
    +1= right/down x/y
    
    TODO:
    Target nodes: something like...
    i in range (4):
    i is even = board [x,y+value*target_matrix]
    is is odd = board [x+value*target_matrx,y]

--- TGS.py
+++ TGS.py
@@ -1,25 +1,36 @@
+from collections import namedtuple
 class Node:
     def __init__(self, value):
         self.value = value
         self.active = True
+        self.targets = [self.value * target_matrix[i] for i in range(4)]
+        self.location = namedtuple('Point', ['x','y'] )
+    
     def __repr__(self):
-        return str(self.value)        
+        return str(self.value)    
+
+    #def add_targets(self, board):        
+    #    for i in range(4):            
+    #        self.targets[i] = self.value * target_matrix[i]
+
+# clockwise: up, right, down, left
+# multiply matrix by node value to determine target node to assign to targets list
+target_matrix = [-1, 1, 1, -1]
 
 level =  [[2, 1, 2, 2], [2, 2, 2, 1], [1, 1, 1, 2], [1, 2, 3, 1] ]
-board = []
+board = [[Node(col) for col in row] for row in level]
 
-for row in level:
-    r = []    
-    for val in row:
-        r.append(Node(val))
-    board.append(r)
+
 
 for row in board:
     for node in row:
         print(node, end=" ")
     print()
 
-# List Comprehension Form
-#b = [[Node(col) for col in row] for row in level]
-#print(*b[col],sep="\n")
-
+#for row in board:    
+#    for node in row:
+#        print(node, ":")
+#        for i in range(4):
+#            print(node.targets[i], end=",")
+#        print()
+        
