c0aef7888a7028baee0e9957c9b98754a4ac6705
symbioid <symbioid@gmail.com>
2020-08-16 13:37:23 -0500

    remove TGS duplicate. starting Walker class (errors out/broken)

--- TetraGraphSolver.py
+++ TetraGraphSolver.py
@@ -3,17 +3,17 @@ from collections import namedtuple
 class Node:
     def __init__(self, value, r, c):
         self.value = value
-        self.active = True
+        self.visited = False
         self.targets = []
         Position = namedtuple('Position', ['row','col'] )
         self.location = Position(r,c)
-    
+
     def __repr__(self):
-        return str(self.value)    
+        return str(self.value)
 
 class Board:
-    board = []        
-    def __init__(self, level):        
+    board = []
+    def __init__(self, level):
         # Make Board
         for r,row in enumerate(level):
             board_row = []
@@ -23,30 +23,41 @@ class Board:
         # Add Targets to each Node
         for row in self.board:
             for node in row:
-                for i in range(4):            
+                for i in range(4):
                     row_displaced = node.location.row + node.value * direction_matrix[i]
                     col_displaced = node.location.col + node.value * direction_matrix[i]
                     if (row_displaced < 0) or (row_displaced > 3) or (col_displaced < 0 ) or (col_displaced > 3):
                         node.targets.append(None)
-                    elif i % 2 == 0:                
+                    elif i % 2 == 0:
                         node.targets.append(self.board[row_displaced][node.location.col])
-                    else:                                
-                        node.targets.append(self.board[node.location.row][col_displaced])                        
+                    else:
+                        node.targets.append(self.board[node.location.row][col_displaced])
     def display(self):
         # Display Node Info
-        for row in self.board:    
+        for row in self.board:
             for node in row:
                 print("----------------")
                 print("Node ({},{}) = [{}] :".format(node.location.row, node.location.col, node))
                 print("----------------")
                 for i, target in enumerate(node.targets):
                     if target is None:
-                        print ("Target {}: (NONE)".format(i))                
+                        print ("Target {}: (NONE)".format(i))
                     else:
-                        print("Target {}: ({},{})".format(i, target.location.row, target.location.col))                   
-            
+                        print("Target {}: ({},{})".format(i, target.location.row, target.location.col))
+
+class Walker:
+    startnode = None
+    def __init__(self, sn):
+        self.startnode = sn
+    def __repr__(self):
+        for target in self.startnode.targets:
+            print(target)
+
 direction_matrix = [-1, 1, 1, -1]
 level =  [[2, 2, 2, 1], [2, 1, 1, 1], [1, 2, 2, 1], [2, 1, 1, 2]]
 
 b = Board(level)
+w = Walker(b.board[3][3])
+
 b.display()
+print(w)
