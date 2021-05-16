9b6dca5f6012e425100975aa4d43ba35529fef2d
symbioid <symbioid@gmail.com>
2021-05-16 18:37:26 -0500

    Show actual board layout; Begin setting up to scan targets in Walker class.

--- TetraGraphSolver.py
+++ TetraGraphSolver.py
@@ -5,17 +5,18 @@ class Node:
         self.value = value
         self.visited = False
         self.targets = []
-        Location = namedtuple('Locaton', ['row','col'] )
+        Location = namedtuple('Location', ['row','col'] )
         self.location = Location(r,c)
+        self.lastdirection = 0
 
     def __repr__(self):
         return str(self.value)
 
 class Board:
     board = []
-    def __init__(self, level):
+    def __init__(self, levelname):
         # Make Board
-        for r,row in enumerate(level):
+        for r,row in enumerate(levelname):
             board_row = []
             for c,value in enumerate(row):
                 board_row.append(Node(value, r, c))
@@ -32,32 +33,49 @@ class Board:
                         node.targets.append(self.board[row_displaced][node.location.col])
                     else:
                         node.targets.append(self.board[node.location.row][col_displaced])
+
     def display(self):
         # Display Node Info
         for row in self.board:
-            for node in row:
-                print("----------------")
-                print("Node ({},{}) = [{}] :".format(node.location.row, node.location.col, node))
-                print("----------------")
-                for i, target in enumerate(node.targets):
-                    if target is None:
-                        print ("Target {}: (NONE)".format(i))
-                    else:
-                        print("Target {}: ({},{})".format(i, target.location.row, target.location.col))
-                print("----------------")
+            print(*row)
+
+#---------------------------------------------------------------------------------
 
 class Walker:
     current_node = None
+    current_target = None
+    index = 0
+
     def __init__(self, start_node):
         self.current_node = start_node
-    
+        self.current_node.visited = True
+        self.current_target = start_node.targets[self.index]
     def display(self):
         print ("Current Node = [{}, {}], with Value of '{}'".format(self.current_node.location.row, self.current_node.location.col, self.current_node.value))
         for target in self.current_node.targets:
             if target == None:
                 print("None")
             else:
-                print("Current Value: {}, Current Target {}".format(target.value, target.location))
+                print("Current Target {}, Current Value: {}".format(target.location, target.value))
+    def scan(self):
+    	print("New Target = {}".format(self.current_node.targets[self.index].location))
+    	if self.current_target == None:
+    		print("None") #pass/return/break
+    	elif self.current_target.visited == True:
+    	    print("Already visited : {}".format(self.current_target.location))
+    	else:
+    	    self.visit()
+    	#self.index += 1
+    	#print("New Target = {}".format(self.current_node.targets[self.index]))
+
+    def visit(self):
+        self.current_node.lastdirection += 1
+        self.current_node = self.current_target #focus on this next...
+        self.current_node.visited = True
+        self.current_target = self.current_node.targets[self.current_node.lastdirection]
+#---------------------------------------------------------------------------------
+
+solutions =[]
 
 direction_matrix = [-1, 1, 1, -1]
 level =  [[2, 2, 2, 1], [2, 1, 1, 1], [1, 2, 2, 1], [2, 1, 1, 2]]
@@ -66,4 +84,6 @@ b = Board(level)
 w = Walker(b.board[3][3])
 
 b.display()
+w.display()
+w.scan()
 w.display()
