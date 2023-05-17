58e9066895344cb661d669e74cb2d823273a3c9e
symbioid <symbioid@gmail.com>
2023-05-16 23:54:47 -0500

    fixed some shit, deal with it later

--- TetraGraphSolver.py
+++ TetraGraphSolver.py
@@ -22,6 +22,7 @@ class Node:
         self.location = Location(r, c)
         self.direction = 0
         self.all_paths_found_from_here = False
+        self.visited_from_direction
 
     def __repr__(self):
         return str(self.value)
@@ -31,7 +32,6 @@ class Node:
 
     def set_direction(self, value):
         self.direction = value
-
 
 class Board:
     board = []
@@ -89,7 +89,6 @@ class Board:
                         print("NONE")
         print()
 
-
 class Walker:
     start_node = None
     current_node = None
@@ -107,9 +106,10 @@ class Walker:
         self.current_path.append(self.start_node)
 
     def scan(self):
-        print(f"Scanning:[{self.current_node.location.row}][{self.current_node.direction}]: {self.current_node.direction}")
+        print(f"Scanning:[{self.current_node.location.row}][{self.current_node.location.col}]: {self.current_node.direction}")
 
         if (self.current_node.direction >= num_poly_sides):
+            print("End of Path, Appending to list of Paths")
             all_paths.append(copy.deepcopy(self.current_path))
             #for p in a:
             #    print(f"Printing all Paths: [{p.location.row}][{p.location.col}]")
@@ -120,9 +120,7 @@ class Walker:
         ) or (self.current_node.targets[self.current_node.direction].visited):
             print("Target null/visited")
             self.turn_right()
-            print("Node[{}][{}] Direction -> {}".format(
-                self.current_node.location.row, self.current_node.location.col,
-                self.current_node.direction))
+            print(f"Node[{self.current_node.location.row}][{self.current_node.location.col}] Direction -> {self.current_node.direction}")
             print()
             self.scan()
         else:
@@ -138,12 +136,12 @@ class Walker:
 
 
 #THIS RIGHT HERE
-
     def turn_right(self):
         print("Turning Right...")
         self.current_node.set_direction(self.current_node.direction + 1)
 
     def move(self):
+        self.prev_node = self.current_node
         self.current_node.last_visited = self.current_node.targets[
             self.current_node.direction]
         self.current_node = self.current_node.targets[
@@ -156,9 +154,11 @@ class Walker:
             f"Backtracking to [{self.prev_node.location.row}][{self.prev_node.location.row}]"
         )
         self.current_node = self.prev_node
+        a = self.current_path.pop()
+        print(a)
         if self.current_node == self.start_node:  # AND direction = 4
             self.current_node.all_paths_found_from_here = True
-            print("all paths found")
+            print(f"all paths found from {self.start_node}")
             return
         else:
             self.scan()
