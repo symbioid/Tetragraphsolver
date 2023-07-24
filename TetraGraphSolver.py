64d82cd90a867abd8731d16339e1b9ebd3b67673
symbioid <symbioid@gmail.com>
2023-07-24 13:42:35 -0500

    Stopping at first node now. But continuing to turn right. still need to fix "backtracking to [2][2]

--- TetraGraphSolver.py
+++ TetraGraphSolver.py
@@ -4,8 +4,8 @@ import copy
 #ADDING LAST_VISTED TO NODE DEF SO WE CAN PROPERLY RE_ROTATE TO NEXT POSITION FROM PREVIOUS (INSTEAD OF LOOPING FROM CURRENT)
 direction_matrix = [-1, 1, 1, -1]
 level = [[2, 1, 1], [1, 1, 2], [1, 1, 2]]
-row_width = 3
-col_height = 3
+ROW_WIDTH = 3
+COL_HEIGHT = 3
 all_paths = [[]]
 num_poly_sides = 4
 
@@ -37,8 +37,8 @@ class Board:
     target_col = 0
 
     # Make Board
-    def __init__(self, l):
-        for r, row in enumerate(l):
+    def __init__(self, width):
+        for r, row in enumerate(width):
             board_row = []
             for c, value in enumerate(row):
                 board_row.append(Node(value, r, c))
@@ -46,8 +46,8 @@ class Board:
 
 
     # Add Targets to each Node
-        for r in range(col_height):
-            for c in range(row_width):
+        for r in range(COL_HEIGHT):
+            for c in range(ROW_WIDTH):
                 for i in range(
                         num_poly_sides
                 ):  #number of sides of polygon (square in this case, hexagon in hexbon/bee game)
@@ -60,9 +60,9 @@ class Board:
                         self.target_col = c + self.board[r][
                             c].value * direction_matrix[i]
                     if ((self.target_row < 0)
-                            or (self.target_row >= col_height)
+                            or (self.target_row >= COL_HEIGHT)
                             or (self.target_col < 0)
-                            or (self.target_col >= row_width)):
+                            or (self.target_col >= ROW_WIDTH)):
                         self.board[r][c].targets.append(None)
                     else:
                         self.board[r][c].targets.append(
@@ -78,8 +78,7 @@ class Board:
                 for i in range(num_poly_sides):
                     if self.board[r][c].targets[i] is not None:
                         node = self.board[r][c]
-                        print(
-                            "Node f[{r}][{c}] -> Target {i} : [{node.targets[i].location.row}][{node.targets[i].location.col}],Value -> {node.targets[i].value}")
+                        print(f"Node [{r}][{c}] -> Target {i} : [{node.targets[i].location.row}][{node.targets[i].location.col}],Value -> {node.targets[i].value}")
                     else:
                         print("NONE")
         print()
@@ -96,7 +95,7 @@ class Walker:
         self.start_node = board[row][col]
         self.start_node.visited = True
         self.current_node = self.start_node
-        self.prev_node = self.start_node
+        self.prev_node = None
         self.current_node.visited = True
         self.current_path.append(self.start_node)
 
@@ -128,7 +127,7 @@ class Walker:
                 self.current_node.location.col))
             print("PATH SO FAR:")
             for node in self.current_path:
-                print("[{}],[{}] ".format(node.location.row, node.location.col))
+                print(f"[{node.location.row}],[{node.location.col}] ")
             self.scan()
 
 
@@ -138,8 +137,9 @@ class Walker:
 
     def move(self):
         self.prev_node = self.current_node
-        self.current_node.last_visited = self.current_node.targets[
-            self.current_node.direction]
+        if self.current_node is not self.start_node:
+            self.current_node.last_visited = self.current_node.targets[
+                self.current_node.direction]
         self.current_node = self.current_node.targets[
             self.current_node.direction]
         self.current_node.visited = True
@@ -148,17 +148,19 @@ class Walker:
 
 #PREVNODE stuck at [2,2]
     def backtrack(self):
-        print(f"Backtracking to [{self.prev_node.location.row}][{self.prev_node.location.row}]")
         self.current_node = self.prev_node
-        a = self.current_path.pop()
-        print(f"this is node popped: {a.location.row}, {a.location.col}")
-        print("After Popping, Path is: ")
-        for idx, n in enumerate(self.current_path):
-            print(f"{idx}: [{n.location.row},{n.location.col}]")
+        print(f"Current Node is Now : [{self.current_node.location.row}][{self.current_node.location.col}]")
+        if self.current_node != self.start_node :
+            print(f"Backtracking to [{self.prev_node.location.row}][{self.prev_node.location.col}]")
+            a = self.current_path.pop()
+            print(f"this is node popped: {a.location.row}, {a.location.col}")
+            print("After Popping, Path is: ")
+            for idx, n in enumerate(self.current_path):
+                print(f"{idx}: [{n.location.row},{n.location.col}]")
 
-        if self.current_node == self.start_node:  # AND direction = 4
+        if len(self.current_path) == 0:  # AND direction = 4
             self.current_node.all_paths_found_from_here = True
-            print(f"all paths found from {self.start_node}")
+            print(f"all paths found from [{self.start_node.location.row}][{self.start_node.location.col}]")
             return
         else:
             self.scan()
