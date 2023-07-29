792e9dcf232f3da3c0bad14a100ca5baaf44feb3
symbioid <symbioid@gmail.com>
2023-07-29 16:30:46 -0500

    openai gen'd docstrings

--- TetraGraphSolver.py
+++ TetraGraphSolver.py
@@ -1,31 +1,18 @@
 from collections import namedtuple
 import copy
 import os
-
 direction_matrix = [-1, 1, 1, -1]
 level = [[2, 1, 1], [1, 1, 2], [1, 1, 2]]
 ROW_WIDTH = 3
 COL_HEIGHT = 3
 NUM_POLY_SIDES = 4
 all_paths = [[]]
-
 class Node:
-    '''This class represents a node in a graph.
+    '''A class to represent a node in a grid.
 
-    Attributes:
-    - last_visited: A reference to the last visited node from this node. (Default: None)
-    - value: The value of the node.
-    - visited: A boolean indicating if the node has been visited or not. (Default: False)
-    - targets: A list of nodes that can be reached from this node.
-    - location: A namedtuple representing the location of the node in the graph, with 'row' and 'col' attributes.
-    - direction: The direction of the node. (Default: 0)
-    - all_paths_found_from_here: A boolean indicating if all paths from this node have been found. (Default: False)
+    Attributes: last_visited (Node): The previous node visited by the search algorithm. value (int): The value of the node. visited (bool): A flag to indicate whether the node has been visited or not. targets (list of Node): A list of nodes that are reachable from this node. location (namedtuple): A namedtuple with row and col attributes to store the coordinates of the node. direction (int): An integer to indicate the direction of the search algorithm (0: right, 1: down, 2: left, 3: up). all_paths_found_from_here (bool): A flag to indicate whether all possible paths from this node have been explored or not.'''
 
-    Methods:
-    - __repr__: Returns a string representation of the node.
-    - get_direction: Returns the direction of the node.
-    - set_direction: Sets the direction of the node.'''
-
+    Methods: init(self, value, row, col): Initializes a Node object with the given value and location. repr(self): Returns a string representation of the node value. get_direction(self): Returns the direction attribute of the node. set_direction(self, value): Sets the direction attribute of the node to the given value. “”"
     def __init__(self, value, row, col):
         self.last_visited = None
         self.value = value
@@ -35,60 +22,27 @@ class Node:
         self.location = Location(row, col)
         self.direction = 0
         self.all_paths_found_from_here = False
-
-
     def __repr__(self):
         return str(self.value)
-
     def get_direction(self):
         return self.direction
-
     def set_direction(self, value):
         self.direction = value
-
 class Board:
-    """
-    This class represents a game board.
-    Attributes:
+    ''' A class to represent a board of nodes in a grid.
 
-    size: The size of the board (number of rows and columns).
-    graph: A dictionary representing the graph structure of the board, where the keys are the nodes and the values are the corresponding node objects.
-    obstacles: A list of nodes that represent obstacles on the board.
-    start: The starting node of the board.
-    end: The ending node of the board.
-    Methods:
+    Attributes: board (list of list of Node): A two-dimensional list of Node objects that form the grid. target_row (int): The row index of the target node for the current node. target_col (int): The column index of the target node for the current node.
 
-    init: Initializes the board object.
-    build_graph: Builds the graph structure of the board.
-    add_obstacle: Adds an obstacle to the board.
-    remove_obstacle: Removes an obstacle from the board.
-    set_start: Sets the starting node of the board.
-    set_end: Sets the ending node of the board.
-    get_node: Returns the node at a specific location on the board.
-    get_neighbors: Returns the neighbors of a given node.
-    get_distance: Returns the distance between two nodes on the board.
-    is_valid_location: Checks if a location is valid on the board.
-    is_valid_move: Checks if a move is valid on the board.
-    reset: Resets the board by clearing the visited and last_visited attributes of each node.
-    find_shortest_path: Finds the shortest path from the start node to the end node on the board using Dijkstra's algorithm.
-    mark_all_paths_found_from_start: Marks all paths from the start node as found.
-    mark_all_paths_found_from_here: Marks all paths from a given node as found.
-    generate_random_obstacles: Generates a random number of obstacles on the board.
-    """
+    Methods: init(self, width): Initializes a Board object with the given width, which is a list of lists of integers representing the values of the nodes. It also creates the Node objects and assigns them their targets based on the direction_matrix and the board dimensions. display(self): Prints the board to the standard output, showing the values of each node. show_targets(self): Prints the targets of each node to the standard output, showing their coordinates and values. If a target is None, it means that it is out of bounds. '''
     board = []
     target_row = 0
     target_col = 0
-
-    # Make Board
     def __init__(self, width):
         for r, row in enumerate(width):
             board_row = []
             for c, value in enumerate(row):
                 board_row.append(Node(value, r, c))
             self.board.append(board_row)
-
-
-    # Add Targets to each Node
         for r in range(COL_HEIGHT):
             for c in range(ROW_WIDTH):
                 for i in range(NUM_POLY_SIDES):
@@ -108,11 +62,9 @@ class Board:
                     else:
                         self.board[r][c].targets.append(
                             self.board[self.target_row][self.target_col])
-
     def display(self):
         for row in self.board:
             print(*row)
-
     def show_targets(self):
         for r, row in enumerate(self.board):
             for c, node in enumerate(row):
@@ -123,42 +75,25 @@ class Board:
                     else:
                         print("NONE")
         print()
-
 class Walker:
+    '''A class to represent a walker that traverses the board of nodes in a grid.
+
+    Attributes: start_node (Node): The node where the walker starts its journey. current_node (Node): The node where the walker is currently located. prev_node (Node): The node where the walker was previously located. current_path (list of Node): A list of nodes that form the current path of the walker. direction (int): An integer to indicate the direction of the walker (0: right, 1: down, 2: left, 3: up).
+
+    Methods: init(self, board, row, col): Initializes a Walker object with the given board, which is a two-dimensional list of Node objects, and the row and col indices of the start node. It also marks the start node as visited and adds it to the current path. scan(self): Scans the target node of the current node based on its direction. If the target node is None or visited, it turns right and scans again. If the target node is valid, it moves to it and scans again. If the current node has exhausted all its directions, it appends the current path to the global list of paths and backtracks to the previous node. turn_right(self): Turns the walker right by incrementing its direction attribute by one. move(self): Moves the walker to its target node based on its direction attribute. It also updates the prev_node, current_node, and current_path attributes accordingly. backtrack(self): Moves the walker back to its previous node. It also pops the last node from the current path and scans again. If the current node is the start node, it sets its all_paths_found_from_here attribute to True and prints all the paths found from it. walk(self): Starts the walking process by calling scan and move recursively until all paths are found or exhausted. '''
     start_node = None
     current_node = None
     prev_node = None
     current_path = []
     direction = 0
-
     def __init__(self, board, row, col):
-        '''
-        Initializes a walker object with a start node on the given board at the specified row and column.
-        Sets the start node as visited and sets it as the current node.
-        Adds the start node to the current path.
-        '''
         self.start_node = board[row][col]
         self.start_node.visited = True
         self.current_node = self.start_node
         self.prev_node = None
         self.current_node.visited = True
         self.current_path.append(self.start_node)
-
     def scan(self):
-        '''
-        Performs scanning at the current node.
-        - If the current node's direction exceeds the number of polygon sides, the end of the path is reached,
-        and the current path is appended to the list of paths.
-        Then, the walker backtracks, turns right, and scans again.
-        If all paths have already been found from the current node, the program exits.
-        - If the current node's target at the current direction is None or has been visited,
-        the walker turns right and scans again.
-        - If the current node's target at the current direction is valid and unvisited,
-        the walker moves to that target node, updates the current node and path,
-        and scans again.
-        '''
-        print(f"Scanning Node: [{self.current_node.location.row}][{self.current_node.location.col}] Tgt: {self.current_node.direction}")
-
         if (self.current_node.direction >= NUM_POLY_SIDES):
             print("End of Path, Appending to list of Paths")
             all_paths.append(self.current_path)
@@ -179,81 +114,42 @@ class Walker:
         else:
             print("Valid Target. Moving...")
             self.move()
-            print("Moved to Node[{}][{}]".format(
-                self.current_node.location.row,
-                self.current_node.location.col))
-            print("PATH SO FAR:")
             for node in self.current_path:
                 print(f"[{node.location.row}],[{node.location.col}] ")
             self.scan()
-
-
     def turn_right(self):
-        '''
-        Turns the walker to the right by incrementing the current node's direction by 1.
-        '''
         print(f"Turning RIGHT! [{self.current_node.direction}] -> [{self.current_node.direction + 1}]")
         self.current_node.set_direction(self.current_node.direction + 1)
-        print(f"Node[{self.current_node.location.row}][{self.current_node.location.col}] Direction -> {self.current_node.direction}")
-    print()
-
-
     def move(self):
-        '''
-        Moves the walker to the next target node in the current direction.
-        Updates the previous node to the current node and marks the current node as visited.
-        Appends the current node to the current path.
-        '''
         self.prev_node = self.current_node
         if self.current_node is not self.start_node:
             self.current_node.last_visited = self.current_node.targets[
                 self.current_node.direction]
-        self.current_node = self.current_node.targets[
-            self.current_node.direction]
+        self.current_node = self.current_node.targets[self.current_node.direction]
         self.current_node.visited = True
         self.current_path.append(copy.deepcopy(self.current_node))
-
-
     def backtrack(self):
-        '''
-        Backtracks the walker to the previous node.
-        If the current node is not the start node, pops the last node from the current path and prints
-        the updated path.
-        If the current path is empty, sets the flag indicating all paths have been found from the start node.
-        Prints all the found paths.
-        Recursively scans from the current node again.
-        '''
         self.current_node = self.prev_node
         print('\n')
-
-        if self.current_node != self.start_node :
-            print(f"Backtracking to - [{self.current_path[-1].location.row}][{self.current_path[-1].location.col}] !")
+        if self.current_node != self.start_node:
             a = self.current_path.pop()
             print(f"POPPING: : [{a.location.row}], [{a.location.col}]")
             print("After Backtrack - Path is : ")
             for idx, n in enumerate(self.current_path):
                 print(f"{idx}: [{n.location.row},{n.location.col}]")
-
         if len(self.current_path) == 0:
             self.current_node.all_paths_found_from_here = True
             print(f"all paths found from [{self.start_node.location.row}][{self.start_node.location.col}]")
             for idx, p in enumerate(all_paths):
                 for n in p:
                     print(f"Printing Path[{idx}]: [{n.location.row}][{n.location.col}]")
-            print("\n")
             return
         self.scan()
         return
-
     def walk(self):
-        '''
-        Initiates the walker's traversal by scanning and moving to the next node.
-        Recursively continues the walking process.
-        '''
         self.scan()
         self.move()
         self.walk()
-
 os.system('clear')
 b = Board(level)
 b.display()
