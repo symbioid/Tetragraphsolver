666a7ac4027be8aa4a6a1e824a40846870e9c505
symbioid <symbioid@gmail.com>
2024-11-12 00:06:02 -0600

    Adding networkx code to start building graph without traversal for now.
    Next work on traversal.

--- TGS2.py
+++ TGS2.py
@@ -1,14 +1,19 @@
 from collections import namedtuple
 import copy
-import os, sys
+
+import os
+import sys
+import networkx as nx
+import scipy
+G = nx.DiGraph()
 
 direction_matrix = [-1, 1, 1, -1]
 adjacent_direction_inversion = [2, 3, 0, 1]
-# targetnode.visited_by_direction[adjacent_direction_inversion[current_node.direction]] = True then move
 level = [[2, 1, 1], [1, 1, 2], [1, 1, 2]]
 ROW_WIDTH = 3
 COL_HEIGHT = 3
 NUM_POLY_SIDES = 4
+
 all_paths = [[]]
 
 
@@ -21,17 +26,12 @@ class Node:
         self.location = Location(row, col)
         self.ident = str(row) + str(col)
         self.direction = 0
-
-        # "visited_by_node_counter" is a dictionary of nodes that have visited this node
-        # After first pass of board build, follow each node's edges to the targets, then...
-        # Insert current_node.ident into target_node.visited_by_node_counter and value of True
-
         self.visited_by_node_counter = {}
         self.visited = False
         self.all_paths_found_from_here = False
 
     def __repr__(self):
-        return str(self.value)
+        return f"({self.ident}): {self.value}"
 
 
 class Board:
@@ -72,13 +72,23 @@ class Board:
                         self.board[r][c].targets.append(
                             self.board[self.target_row][self.target_col]
                         )
-                        self.board[r][c].targets[i].visited_by_node_counter[
-                            "self.ident"
-                        ] = 0
+                        G.add_edge(
+                            self.board[r][c],
+                            self.board[self.target_row][self.target_col],
+                        )
+            print([(n, nbrdict) for n, nbrdict in G.adjacency()])
+
+            # print(
+            #    f"Source = {self.board[r][c].ident} | Target: {self.board[self.target_row][self.target_col].ident}"
+            # )
 
     def display(self):
-        for row in self.board:
-            print(*row)
+        # for i, row in enumerate(self.board):
+        #    print(*row)
+        # for j, col in enumerate(row):
+        #    print(descendants(G, self.board[i][j].ident))
+        for i, g in G.out_edges:
+            print(f"G.out_edges ({i}): {g}")
 
     def show_targets(self):
         for r, row in enumerate(self.board):
@@ -108,21 +118,19 @@ class Walker:
         self.current_node = self.start_node
         self.current_path.append(self.start_node)
 
+    #        print(f"{self.start_node.ident}")
+    #        print(f"Graph: {G.nodes}")
+
     def scan(self):
-        # TODO: Return the target of the current node's direction
         print(
             f"Scanning:[{self.current_node.location.row}][{self.current_node.location.col}]: {self.current_node.direction}"
         )
-
-        # End condition, but the real question: Why, when backtracking TO 2,1 does it not revert current and then increment direction (turn right from most recent)
-        # Once again - Need to store the direction of the last node visited ON THE NODE ITSELF so when backing up can increment that.  Not from the last visted version/edit of that node on the list.(wait too tired, what do i even mean on this. fuck. time for vampire survivors.)
-        # OR update list with deepcopy after backtracking
         if self.current_node.direction == NUM_POLY_SIDES:
             print("End of Path, Appending to list of Paths")
             all_paths.append(copy.deepcopy(self.current_path))
-            for idx, p in enumerate(all_paths):
-                for n in p:
-                    print(f"Printing Path[{idx}]: [{n.location.row}][{n.location.col}]")
+            # for idx, p in enumerate(all_paths):
+            #    for n in p:
+            #        print(f"Printing Path[{idx}]: [{n.location.row}][{n.location.col}]")
             print("\n")
             self.backtrack()
             if self.current_node.all_paths_found_from_here:
@@ -132,39 +140,43 @@ class Walker:
         ):
             print("Target None/Visited")
             self.turn_right()
-            print(
-                f"Node[{self.current_node.location.row}][{self.current_node.location.col}] Direction -> {self.current_node.direction}"
-            )
+            # print(
+            #     f"Node[{self.current_node.location.row}][{self.current_node.location.col}] Direction -> {self.current_node.direction}"
+            # )
             print()
             self.scan()
         else:
             print("Valid Target. Moving...")
             self.move()
-            print(
-                f"Moved to Node[{self.current_node.location.row}][{self.current_node.location.col}]"
-            )
+            # print(
+            #     f"Moved to Node[{self.current_node.location.row}][{self.current_node.location.col}]"
+            # )
             print("PATH SO FAR:")
             for node in self.current_path:
                 print(f"[{node.location.row}],[{node.location.col}] ")
             self.scan()
 
     def turn_right(self):
-        print(
-            f"Turning RIGHT! [{self.current_node.direction}] -> [{self.current_node.direction + 1}]"
-        )
+        # print(
+        #     f"Turning RIGHT! [{self.current_node.direction}] -> [{self.current_node.direction + 1}]"
+        # )
         self.current_node.direction += 1
 
     def move(self):
         self.prev_node = self.current_node
+
         if self.current_node is not self.start_node:
             self.current_node.last_visited = self.current_node.targets[
                 self.current_node.direction
             ]
+
         self.current_node = self.current_node.targets[self.current_node.direction]
         self.current_node.visited = True
 
         # APPENDING PATH SHOULD NOT BE PART OF THE MOVE PROCEDURE
         self.current_path.append(copy.copy(self.current_node))
+        # G.add_node(self.current_node.ident)
+        # print(G.nodes)
 
     def backtrack(self):
         print(
@@ -172,9 +184,9 @@ class Walker:
         )
         if len(self.current_path) == 0:
             self.current_node.all_paths_found_from_here = True
-            print(
-                f"all paths found from [{self.start_node.location.row}][{self.start_node.location.col}]"
-            )
+            # print(
+            #     f"all paths found from [{self.start_node.location.row}][{self.start_node.location.col}]"
+            # )
         elif self.current_node != self.start_node:
             print("Backtracking...")
             # NOT THIS BUT THERE NEEDS TO BE A RECOPY OF THE BOARD NODE ITSELF + LAST_VISITED
@@ -182,9 +194,9 @@ class Walker:
 
             self.current_node = copy.deepcopy(self.prev_node)
             a = self.current_path.pop()
-            print(f"POPPING: : [{a.location.row}][{a.location.col}]")
+            # print(f"POPPING: : [{a.location.row}][{a.location.col}]")
             self.current_node.visited = True
-            print("After Popping, Path is: ")
+            # print("After Popping, Path is: ")
             for idx, n in enumerate(self.current_path):
                 print(f"{idx}: [{n.location.row},{n.location.col}]")
             # self.scan()
@@ -198,6 +210,6 @@ class Walker:
 os.system("clear")
 b = Board(level)
 b.display()
-w = Walker(b.board, 0, 0)
+# w = Walker(b.board, 0, 0)
 # b.show_targets()
-w.walk()
+# w.walk()
