8c5a9493432a0238d355fd135b8fdb0edf8882d8
symbioid <symbioid@gmail.com>
2025-03-01 17:53:30 -0600

    Using networkx and added coloring and some more to walker. need proper backtracking implemented.  but rewrite is making progress.

--- TGS2networkx1.py
+++ TGS2networkx1.py
@@ -1,17 +1,25 @@
 from collections import namedtuple
+
+# from turtle import clearscreen
+
+# import copy
+
+# import os
+# import sys
 import networkx as nx
+
+# from networkx.algorithms import tournament
 
 G = nx.DiGraph()
 
 direction_matrix = [-1, 1, 1, -1]
-# adjacent_direction_inversion = [2, 3, 0, 1]
+adjacent_direction_inversion = [2, 3, 0, 1]
 level = [[2, 1, 1], [1, 1, 2], [1, 1, 2]]
 ROW_WIDTH = 3
 COL_HEIGHT = 3
 NUM_POLY_SIDES = 4
 current_path = []
 all_paths = [[]]
-Location = namedtuple("Location", ["row", "col"])
 
 
 class Node:
@@ -22,8 +30,7 @@ class Node:
         self.value = value
         self.targets = []
         self.visited = False
-        self.direction = 0
-        self.visited_by = []
+        self.last_visited_direction = 0
 
     def __repr__(self):
         return f"{self.ident.split(' ')}"
@@ -31,9 +38,11 @@ class Node:
 
 class Board:
     board = []
+    target_row = 0
+    target_col = 0
 
     # Make Board
-    def __init__(self, level, row, col):
+    def __init__(self, level):
         for r, row in enumerate(level):
             board_row = []
             for c, value in enumerate(row):
@@ -41,7 +50,6 @@ class Board:
                 board_row.append(n)
                 G.add_node(n)
             self.board.append(board_row)
-
         # Add Targets to each Node
         for r in range(COL_HEIGHT):
             for c in range(ROW_WIDTH):
@@ -72,20 +80,12 @@ class Board:
                             self.board[self.target_row][self.target_col],
                         )
 
-    def display(self, active):
-        defaultcolor = "\033[0m"
-        activecolor = "\033[33m"
-        print("-----------------------")
-        print("|   SHOWING BOARD:    |")
-        print("-----------------------")
+    def display(self):
         for row in self.board:
-            for node in row:
-                if node.location == active.location:
-                    print(activecolor + f"{node.value}", end=" ")
-                else:
-                    print(defaultcolor + f"{node.value}", end=" ")
-            print("\033[0m")
-        print("-----------------------")
+            for val in row:
+                # print(*row)
+                print(val, end=" ")
+            print()
 
     def show_targets(self):
         for r, row in enumerate(self.board):
@@ -95,50 +95,11 @@ class Board:
                         f"Node [{r}][{c}] -> Target {i} : [{t.location.row}][{t.location.col}] | Value -> {t.value}"
                     )
                 print("------------")
-        print()
 
 
-class Walker:
-    starting_row = 0
-    starting_col = 0
-
-    board = Board(level, starting_row, starting_col)
-    current_node = board.board[starting_row][starting_col]
-
-    def __init__(self, board, row):
-        self.location = Location(self.starting_row, self.starting_col)
-        self.current_node = self.board.board[self.starting_row][self.starting_col]
-        self.current_node.visited = True
-        self.current_node.visited_by = None
-        current_path.append(self.current_node)
-
-    def show_self(self):
-        self.board.display(self.current_node)
-
-        print(
-            f"walker.current_node.location: {self.current_node.location[0]}, {self.current_node.location[1]}"
-        )
-        print(f"walker.current_node.value: {self.current_node.value}")
-        print("-----------------------")
-
-    def walk(self):
-        for target in self.current_node.targets:
-            if target.visited is False:
-                self.show_self()
-                self.visit(target)
-                current_path.append(target)
-
-    def visit(self, target):
-        print(f"From: {self.current_node.location}")
-        self.current_node.visited = True
-        self.current_node = target
-        print(f"To: {self.current_node.location}")
-        self.walk()
-
-
-w = Walker(0, 1)
-# w.board.display(w.current_node)
-# w.show_self()
-# w.board.show_targets()
-# print(G.nodes)
-w.walk()
+b = Board(level)
+b.display()
+b.show_targets()
+print(G.nodes)
+for line in nx.generate_adjlist(G):
+    print(line)
